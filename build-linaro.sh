18373b301cac498053a57aa49ad70cccbefc5b88
Matt Filetto <matt.filetto@gmail.com>
2013-06-22 20:59:17 -0700

    build-linaro.sh: fix toolchain path
    Change-Id: I3c60804697540001144a6c5e6b8dcd8b224676fd

--- build-linaro.sh
+++ build-linaro.sh
@@ -12,7 +12,7 @@ MAKE="4"
 ## Set compiler location to compile with linaro cortex a8
 echo Setting compiler location...
 export ARCH=arm
-export CROSS_COMPILE=$HOME/android/system/prebuilt/linux-x86/toolchain/linaro/bin/arm-cortex_a8-linux-gnueabi-
+export CROSS_COMPILE=$HOME/android/system/prebuilt/linux-x86/toolchain/linaro-arm-cortex-a8/bin/arm-cortex_a8-linux-gnueabi-
 
 ## Build kernel using pyramid_defconfig
 make mrproper
