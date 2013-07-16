6855a2c1a4c3253b59ed3b2f0e6e7d083a2a9a9a
Matt Filetto <matt.filetto@gmail.com>
2013-07-16 05:08:51 -0700

    prebuilt: update build scripts.
    Everything is now built in <kerneldir>/Packages/
    
    Change-Id: Ie523b81995b5cfaea5ab35049137628801061eb2

--- build-pyramid.sh
+++ build-pyramid.sh
@@ -10,38 +10,40 @@ time_start=$(date +%s.%N)
 MAKE="4"
 
 ## Set compiler location to compile with linaro cortex a8
-echo Setting compiler location...
+echo "Setting compiler location..."
 export ARCH=arm
 export CROSS_COMPILE=$HOME/android/system/prebuilt/linux-x86/toolchain/linaro-arm-cortex-a8/bin/arm-cortex_a8-linux-gnueabi-
 
 ## Build kernel using pyramid_defconfig
 make pyramid_defconfig
-make -j$MAKE ARCH=arm
+make -j`grep 'processor' /proc/cpuinfo | wc -l` ARCH=arm
 sleep 1
 
 # Post compile tasks
-echo Copying compiled kernel and modules to $HOME/KERNEL/
-echo and building flashable zip
+echo "Copying compiled kernel and modules to Packages/out/"
+echo "and building flashable zip"
 sleep 1
 
-     mkdir -p $HOME/KERNEL/
-     mkdir -p $HOME/KERNEL/out/
-     mkdir -p $HOME/KERNEL/out/system/lib/modules/
-     mkdir -p $HOME/KERNEL/out/kernel/
-     mkdir -p $HOME/KERNEL/out/META-INF/
+     mkdir -p Packages/
+     mkdir -p Packages/out/
+     mkdir -p Packages/out/system/lib/modules/
+     mkdir -p Packages/out/kernel/
+     mkdir -p Packages/out/META-INF/
 
-cp -a $(find . -name *.ko -print |grep -v initramfs) $HOME/KERNEL/out/system/lib/modules/
-cp -rf prebuilt-scripts/META-INF/ $HOME/KERNEL/out/
-cp -rf prebuilt-scripts/kernel_dir/* $HOME/KERNEL/out/kernel/
-cp arch/arm/boot/zImage $HOME/KERNEL/out/kernel/
+cp -a $(find . -name *.ko -print |grep -v initramfs) Packages/out/system/lib/modules/
+cp -rf prebuilt-scripts/META-INF/ Packages/out/
+cp -rf prebuilt-scripts/kernel_dir/* Packages/out/kernel/
+cp arch/arm/boot/zImage Packages/out/kernel/
 
 # build flashable zip
+     export curdate=`date "+%m-%d-%Y"`
+     cd Packages/out/
+     zip -r ../pyramid-kernel-dev-$curdate.zip .
+     echo "Deleting Temp files and folders...."
+     cd ../../
+     rm -rf Packages/out/
 
-cd $HOME/KERNEL/out/
-zip -9 -r $HOME/pyramid-kernel-dev-$curdate.zip .
-echo Deleting Temp files and folders....
-rm -rf $HOME/KERNEL/
-echo "Build Complete, Check your Home directory for a flashable zip"
+echo "Build Complete, Check Packages directory for flashable zip"
 time_end=$(date +%s.%N)
 echo -e "${BLDYLW}Total time elapsed: ${TCTCLR}${TXTGRN}$(echo "($time_end - $time_start) / 60"|bc ) ${TXTYLW}minutes${TXTGRN} ($(echo "$time_end - $time_start"|bc ) ${TXTYLW}seconds) ${TXTCLR}"
 
